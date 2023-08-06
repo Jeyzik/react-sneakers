0060fd065fbed410a18104d609d83ec319664a9d
Jeyzik <kojimaneon@gmail.com>
2023-08-06 04:57:42 +0300

    module and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
+
+const arr = [
+  {
+    title: "Nike Blazer Mid Suede Men's Sneakers",
+    price: 120,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    title: "Nike Air Max 270 Men's Sneakers",
+    price: 115,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    title: "Nike Blazer Mid Suede Men's Sneakers",
+    price: 89,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    title: "Puma X Aka Boku Future Rider sneakers",
+    price: 95,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="wrapper clear">
       <Drawer />
-
       <Header />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
@@ -17,7 +39,14 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,22 @@
 import React from "react";
+import styles from "./Card.module.scss";
 
-const Card = () => {
+console.log(styles);
+
+const Card = (props) => {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-      <h5>Nike Blazer Mid Suede Men's Sneakers</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b>120 $</b>
+          <b>{props.price} $</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="plus" />
         </button>
       </div>
