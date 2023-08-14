3c0862bd445820ecdd559eddc913ec447bc4bcc3
Jeyzik <kojimaneon@gmail.com>
2023-08-14 04:01:31 +0300

    add to cart and fix index error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
@@ -6,46 +7,85 @@ import Header from "./components/Header";
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState("");
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch("https://64d0212bffcda80aff526b27.mockapi.io/items")
+    axios
+      .get("https://64d0212bffcda80aff526b27.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
+      });
+    axios
+      .get("https://64d0212bffcda80aff526b27.mockapi.io/cart")
+      .then((res) => {
+        setCartItems(res.data);
       });
   }, []);
 
   const onAddToCart = (obj) => {
+    axios.post("https://64d0212bffcda80aff526b27.mockapi.io/cart", obj);
     setCartItems((prev) => [...prev, obj]);
+  };
+
+  const onRemoveItem = (id) => {
+    // axios.delete(`https://64d0212bffcda80aff526b27.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const onChangeSearchInput = (e) => {
+    setSearchValue(e.target.value);
   };
 
   return (
     <div className="wrapper clear">
       {cartOpened && (
-        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+        <Drawer
+          items={cartItems}
+          onClose={() => setCartOpened(false)}
+          onRemove={onRemoveItem}
+        />
       )}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>All sneakers</h1>
+          <h1>
+            {searchValue
+              ? `Search By Request: "${searchValue}"`
+              : "All sneakers"}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Search..." />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className="clear cu-p"
+                src="/img/btn-remove.svg"
+                alt="clear-btn"
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder="Search..."
+            />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onFavorite={() => console.log("click favorite")}
-              onPlus={onAddToCart}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, i) => (
+              <Card
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onFavorite={() => console.log("click favorite")}
+                onPlus={onAddToCart}
+                key={i}
+              />
+            ))}
         </div>
       </div>
     </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Drawer = ({ onClose, items = [] }) => {
+const Drawer = ({ onClose, onRemove, items = [] }) => {
   return (
     <div className="overlay">
       <div className="drawer">
@@ -14,43 +14,65 @@ const Drawer = ({ onClose, items = [] }) => {
           />
         </h2>
 
-        <div className="items">
-          {items.map((obj) => (
-            <div className="cartItem d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: `url(${obj.imageUrl})` }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">{obj.title}</p>
-                <b>{obj.price} $</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn-remove.svg"
-                alt="remove"
-              />
+        {items.length > 0 ? (
+          <div>
+            <div className="items">
+              {items.map((obj) => (
+                <div className="cartItem d-flex align-center mb-20">
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className="cartItemImg"
+                  ></div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} $</b>
+                  </div>
+                  <img
+                    className="removeBtn"
+                    src="/img/btn-remove.svg"
+                    alt="remove"
+                    onClick={() => onRemove(obj.id)}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-
-        <div className="cartTotalBlock">
-          <ul>
-            <li>
-              <span>Total:</span>
-              <div></div>
-              <b>240 $</b>
-            </li>
-            <li>
-              <span>Tax 5%</span>
-              <div></div>
-              <b>12 $</b>
-            </li>
-          </ul>
-          <button className="greenButton">
-            Checkout <img src="/img/arrow.svg" alt="arrow" />
-          </button>
-        </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Total:</span>
+                  <div></div>
+                  <b>240 $</b>
+                </li>
+                <li>
+                  <span>Tax 5%</span>
+                  <div></div>
+                  <b>12 $</b>
+                </li>
+              </ul>
+              <button className="greenButton">
+                Checkout <img src="/img/arrow.svg" alt="arrow" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+            <img
+              className="mb-20"
+              width={120}
+              height={120}
+              src="/img/cart-empty.png"
+              alt="empty-cart"
+            />
+            <h2>Empty Cart</h2>
+            <p className="opacity-6">
+              Please add at least 1 product to place an order.
+            </p>
+            <button onClick={onClose} className="greenButton">
+              <img src="/img/arrow.svg" alt="arrow" />
+              Back
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
